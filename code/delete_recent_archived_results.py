95a14f31ce9c4717412fdabffad4eb60100a24e8
David Feng <no-reply@codeocean.com>
2024-04-23 15:22:32 +0000

    Version 1.0

---
+++ code/delete_recent_archived_results.py
@@ -0,0 +1,28 @@
+from aind_codeocean_api.codeocean import CodeOceanClient
+from aind_codeocean_utils.api_handler import APIHandler
+import datetime
+import os, sys, argparse
+
+
+def run(delete_after=30, dry_run=True):
+    co_client = CodeOceanClient(domain="https://codeocean.allenneuraldynamics.org/", token=os.environ["API_SECRET"])
+    api_handler = APIHandler(co_client=co_client)
+
+    keep_after = datetime.datetime.now() - datetime.timedelta(days=delete_after)
+
+    assets = api_handler.find_archived_data_assets_to_delete(keep_after=keep_after)
+
+    for i,asset in enumerate(assets):
+        created = datetime.datetime.fromtimestamp(asset['created'])        
+        print(f"{'(dry-run)' if dry_run else ''} deleting {asset['type']} {asset['name']}, created {created}")
+        if not dry_run:
+            co_client.delete_data_asset(data_asset_id=asset['id'])
+
+if __name__ == "__main__": 
+    argparse = argparse.ArgumentParser()
+    argparse.add_argument('--dry-run')
+    argparse.add_argument('--delete-after', type=int)
+    args = argparse.parse_args()
+    
+    dry_run = args.dry_run != "off"
+    run(delete_after=args.delete_after, dry_run=dry_run)
