a6668631334ff0a61cb3af15f19c524c3506fd36
David Feng <no-reply@codeocean.com>
2024-07-02 22:31:18 +0000

    Version 2.0

--- code/delete_recent_archived_results.py
+++ code/delete_recent_archived_results.py
@@ -5,7 +5,7 @@ import os, sys, argparse
 
 
 def run(delete_after=30, dry_run=True):
-    co_client = CodeOceanClient(domain="https://codeocean.allenneuraldynamics.org/", token=os.environ["API_SECRET"])
+    co_client = CodeOceanClient(domain="https://codeocean.allenneuraldynamics.org/", token=os.environ["CUSTOM_KEY"])
     api_handler = APIHandler(co_client=co_client)
 
     keep_after = datetime.datetime.now() - datetime.timedelta(days=delete_after)
